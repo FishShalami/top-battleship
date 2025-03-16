26b26296637d705d059018e886e2c696a8ce266e
FishShalami <fishshalami@gmail.com>
2025-03-15 20:10:08 -0700

    Adding pop-ups to notify user on game status

--- src/index.js
+++ src/index.js
@@ -354,8 +354,11 @@ async function gameLoop(attacker, defender) {
 }
 
 (async function startGameSequence() {
+  alert(`Pick locations for five ships (lengths 2, 3, 3, 4, and 5). 
+    Hold shift key when selecting to make ship vertical`);
   await pickShipCoord();
   placeComputerShips();
   initializeBoard();
+  alert("Time to play! Pick an attack location on the computer's board");
   gameLoop(player1, player2);
 })();
